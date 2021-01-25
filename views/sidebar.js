a135ab303267e1700eb4e28dc4874ce8ac71e3fe
Alma Ayaquica <yogo060@gmail.com>
2021-01-25 14:04:12 -0600

    cambios link & iconos

--- views/sidebar.js
+++ views/sidebar.js
@@ -10,16 +10,16 @@ document.write(`
                     <a href="../pages/dashboard.html"><i class="fa fa-bell"></i> Mis Tareas</a>
                 </li>
                 <li>
-                    <a href="../pages/expediente.html"><i class="fa fa-plus"></i> Nueva tarea</a>
+                    <a href="../pages/nexpediente.html"><i class="fa fa-plus"></i> Nueva tarea</a>
                 </li>
                 <li>
-                    <a href="../pages/contratos.html"><i class="fas fa-file-alt"></i> Alta de Tramite</a>
+                    <a href="../pages/expediente.html"><i class="fas fa-file-alt"></i> Alta de Tramite</a>
                 </li>
                 <li>
                     <a href="../pages/obligaciones.html"><i class="fa fa-check-circle"></i> Seguimiento de Cumplimiento</a>
                 </li>
                 <li>
-                    <a href="#"><i class="fas fa-file-excel"></i> Cumplimientos Contractuales</a>
+                    <a href="../pages/contratos.html"><i class="fas fa-file-excel"></i> Cumplimientos Contractuales</a>
                 </li>
                 <li>
                     <a href="../pages/usuarios.html"><i class="fa fa-user"></i> Usuarios</a>
