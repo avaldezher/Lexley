569e5fc4e68fdb34f5a2ebf73fe83baa1d679266
Alma Ayaquica <yogo060@gmail.com>
2021-01-27 15:22:08 -0600

    modal a página usuario

--- views/nusuario.js
+++ views/nusuario.js
@@ -1,15 +1,5 @@
 document.write(`
-<!-- Modal -->
-                <div class="modal fade" id="fullModal" tabindex="-1" role="dialog">
-                  <div class="modal-dialog modal-lg" >
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title"><i class="fa fa-users fa-2x circle-icon text-white"></i> Nuevo Usuario</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <button type="button" class="btn btn-reg-modal mt-4 data-dismiss="modal"">Guardar</button>
-                            </button>
-                        </div>
-                        <div class="modal-body">
+
                         <form class="form-horizontal">
                             <div class="form-group">
                                 <label class="font-weight-bold" for="name">Nombre Completo</label>
@@ -48,11 +38,5 @@ document.write(`
                                 </div>
                             </div>
                         </form>
-                      </div>
-                      <div class="modal-footer justify-content-center">
-                        <button type="button" class="btn btn-reg-modal ">Guardar</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+
 `);
